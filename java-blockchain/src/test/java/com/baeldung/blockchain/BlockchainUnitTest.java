ae702a028005d18df10ca15cc6ddc3222e515eb1
worka <study.aniket.hingane@gmail.com>
2024-08-06 01:36:25 -0400

    Initial commit

--- java-blockchain/src/test/java/com/baeldung/blockchain/BlockchainUnitTest.java
+++ java-blockchain/src/test/java/com/baeldung/blockchain/BlockchainUnitTest.java
@@ -1,4 +1,4 @@
-package com.baeldung.blockchain;
+package com.baeldung.deepshallowcopy;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
--- java-deep-shallow-copy/src/main/java/com/baeldung/deepshallowcopy/DeepShallowCopyMain.java
+++ java-deep-shallow-copy/src/main/java/com/baeldung/deepshallowcopy/DeepShallowCopyMain.java
@@ -93,8 +93,8 @@ final class ImmutablePerson {
     }
 }
 
-public class Main {
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
+public class DeepShallowCopyMain {
+    private static final Logger logger = Logger.getLogger(DeepShallowCopyMain.class.getName());
 
     public static void main(String[] args) throws CloneNotSupportedException {
         // Shallow Copy
